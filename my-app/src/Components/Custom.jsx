f64fa6dac4bd5fa4704562946fb4ac30bb1ed15b
Florian Jam <jam.florian@gmail.com>
2021-04-05 15:11:22 +0200

    task 2 completed

--- my-app/src/Components/Custom.jsx
+++ my-app/src/Components/Custom.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Custom = () => {
-    return (
-        <div>
-            CUSTOM
-        </div>
-    )
+class Custom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            size: null,
+            url: null,
+            size: null
+        }
+    }
+
+    fetchURL = (e) => {
+        this.setState({url: e.target.value})
+    }
+
+    getSize = (e) => {
+        this.setState({size: e.target.value})
+    }
+
+    render() {
+        const image = this.state.url
+        const imageSize = this.state.size;
+        return (
+            <div>
+                <h1>Customize Image</h1>
+                <div>
+                <input type="text" placeholder='image url' onChange={this.fetchURL}/>
+                </div>
+                <div>
+                <input type="range" min='0' max='200' onChange={this.getSize}/>
+                </div>
+                {image !== null && (
+                    <img style={{width: `${imageSize}px` }} src={image}/>
+                )}
+            </div>
+        )
+    }
 }
 
 
--- my-app/src/Components/Temperature.jsx
+++ my-app/src/Components/Temperature.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Temperature = () => {
-    return (
-        <div>
-           TEMPERATURE
-        </div>
-    )
+class Temperature extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            temperature: null
+        }
+    }
+    
+    handleChange = (e) => {
+        if (e.target.value === '') {
+            this.setState({temperature: null})
+            return
+        }
+        this.setState({temperature: e.target.value})
+    }
+    
+    render() {
+        const temperature = this.state.temperature;
+        return (
+            <div>
+                <h1>Temperature</h1>
+                <input type="number" placeholder='Temperature in Celsius' onChange={this.handleChange}/>
+                {temperature !== null && temperature < 10 && (
+                    <p id='cold'>It's cold ❄️</p>
+                )}
+                {temperature !== null && temperature >= 10 && temperature <= 30  && (
+                    <p id='nice'>It's nice 🌼</p>
+                )}
+                 {temperature !== null && temperature > 30 && (
+                    <p id='warm'>It's warm ☀️</p>
+                )}
+            </div>
+        )
+    }
 }
+
 
 export default Temperature
